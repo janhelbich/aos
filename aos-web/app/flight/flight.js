757b9df33f7dd885408b0678e6321c9a275f14ee
Jan Helbich <jan.helbich@bcvsolutions.eu>
2015-11-25 13:13:05 +0100

    Flights table.

--- aos-web/app/flight/flight.js
+++ aos-web/app/flight/flight.js
@@ -1,14 +1,25 @@
 'use strict';
 
+(function() {
 angular.module('aos.flight', ['ngRoute'])
 
   .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/flight', {
       templateUrl: 'flight/flight.html',
-      controller: 'FlightController'
+      controller: 'FlightController',
+      controllerAs: 'flightCtrl'
     });
   }])
+  .controller('FlightController', ['$scope', '$http', function($scope, $http) {
+    $scope.flights = [ ];
+    
+    $http.get('flight/flight.json').then(function(response) {
+      $scope.flights = response.data;
+      console.log($scope.flights);
+    }, function(msg) {
+      console.log(msg);
+    });
 
-  .controller('FlightController', [function() {
-  
   }]);
+  
+})();
