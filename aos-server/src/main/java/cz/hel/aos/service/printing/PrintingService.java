b60b6e6ff3360f2e9529208f51379ebc7ddabdd4
Jan Helbich <jan.helbich@bcvsolutions.eu>
2015-12-17 06:00:28 +0100

    Created printing service and SoapUI test.

---
+++ aos-server/src/main/java/cz/hel/aos/service/printing/PrintingService.java
@@ -0,0 +1,29 @@
+package cz.hel.aos.service.printing;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import cz.hel.aos.entity.Reservation;
+
+@WebService
+public class PrintingService {
+
+	private static final String NEWLINE = "\n";
+	
+	@WebMethod(action = "printReservation")
+	public String printReservation(Reservation res) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Rezervace cislo. ").append(res.getId()).append(NEWLINE);
+		sb.append("-------------------------------------").append(NEWLINE);
+		sb.append("Pocet mist: ").append(res.getNumberOfSeats()).append(NEWLINE);
+		sb.append("Stav rezervace: ").append(res.getState()).append(NEWLINE);
+		sb.append(NEWLINE);
+		sb.append("-------------------------------------").append(NEWLINE);
+		sb.append("Informace o letu").append(NEWLINE);
+		sb.append("-------------------------------------").append(NEWLINE);
+		sb.append("Nazev letu: ").append(res.getFlight().getFlightName()).append(NEWLINE);
+		
+		
+		return sb.toString();
+	}
+}
